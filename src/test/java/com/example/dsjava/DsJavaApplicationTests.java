a0a49a4e8a11fa6220fcf187699db0f843b4aaf7
Vasilis Papadimitrakopoulos <vasilis.papadimitrakopoulos@ocado.com>
2024-01-14 10:04:20 +0100

    Set up

---
+++ src/test/java/com/example/dsjava/DsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
