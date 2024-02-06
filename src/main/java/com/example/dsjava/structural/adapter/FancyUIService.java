fc6aaa0aceb333e6d620065948a2c34916731f98
Vasilis Papadimitrakopoulos <vasilis.papadimitrakopoulos@ocado.com>
2024-02-06 17:59:02 +0100

    Structural: Adapter

---
+++ src/main/java/com/example/dsjava/structural/adapter/FancyUIService.java
@@ -0,0 +1,9 @@
+package com.example.dsjava.structural.adapter;
+
+import com.example.dsjava.structural.adapter.model.JsonData;
+
+public class FancyUIService {
+    public void displayMenus(JsonData jsonData){
+        // Displays Menus using JSONData
+    }
+}
---
+++ src/main/java/com/example/dsjava/structural/adapter/model/XmlData.java
@@ -0,0 +1,4 @@
+package com.example.dsjava.structural.adapter.model;
+
+public class XmlData {
+}
